c7b603bce38f810028b4a1619caeb07db3b13bef
Craig McClellan <craig@craigmcclellan.com>
2019-01-21 14:46:39 -0600

    Added Contacts Group Script and Readme

---
+++ Save Contacts to Group/README.md
@@ -0,0 +1,5 @@
+# Save Contacts to Group
+
+This (unfinished) script is allows users to create new contact groups on iOS from contacts. If you enter a particular note string (I recommend Emoji) into the note field of a contact, this script will ask for the string and the name of a contact group. If the group exists, it will just add all contacts with that note field to the contact group. If the group doesn't exist, it will create the group, then add the contacts.
+
+I'm posting here now to share and identify bugs.
---
+++ Save Contacts to Group/Save Contacts to Group.js
@@ -0,0 +1,80 @@
+// When run, this Script will search the notes field for all contacts in the default contacts container (usually iCloud) for a character the user designates in the script (I recommend an emoji). Any contacts that have the emoji will be added to the contact group the user specifies. If that group does not exist yet, it will be created.
+
+// Present an alert to the user asking for the search term (emoji) and the name of the Contact Group to add contacts to.
+
+let alert = new Alert();
+alert.title = "Create Contact Group";
+alert.addTextField("Note Search Term");
+alert.addTextField("Contact Group Name");
+alert.addAction("Done");
+alert.addCancelAction("Cancel");
+await alert.present();
+let searchTerm = alert.textFieldValue(0);
+let userGroup = alert.textFieldValue(1);
+
+
+// Gather information on contacts, containers, and groups to be used later in the script.
+
+let container = await ContactsContainer.default();
+let allContacts = await Contact.all([container]);
+let allGroups = await ContactsGroup.all([container])
+
+
+// Determine if Contacts Group already exists, and if not, create it
+
+function checkGroupExists() {
+  var a = 0
+  for (i = 0; i < allGroups.length; i++) {
+    if (allGroups[i].name == userGroup) {
+      a = a + 1;
+     }
+   }
+  if (a == 1) {
+    var b = true;
+    }
+  else if (a == 0) {
+    var b = false;
+   }
+  return b;
+}
+
+function createGroup(groupName) {
+	var newGroup = new ContactsGroup();
+	newGroup.name = groupName;
+ 	ContactsGroup.add(newGroup, container)
+ 	ContactsGroup.update(newGroup)
+ 	Contact.persistChanges();
+}
+
+var groupExists = checkGroupExists();
+
+if (groupExists == false) {
+  createGroup(userGroup);
+  console.log("Group " + userGroup + " created"); 
+}
+else if (groupExists == true) {
+  console.log("Group exists");
+}
+
+// Add Contacts with Emoji Search Term to Contact Group
+
+function addToGroup(groupName) {
+   for (i = 0; i < allGroups.length; i++) {
+      if (allGroups[i].name == userGroup) {
+         var group = allGroups[i];
+      }
+   }
+  for (i = 0; i < allContacts.length; i++) {
+    if (allContacts[i].note == searchTerm) {
+      group.addMember(allContacts[i])
+      ContactsGroup.update(group);
+      allContacts[i].note = "";
+      Contact.update(allContacts[i]);
+      }
+    }
+}
+
+var success = addToGroup(userGroup);
+Contact.persistChanges();
+
+
